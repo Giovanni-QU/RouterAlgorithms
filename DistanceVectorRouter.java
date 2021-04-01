700b7da8d378964718a816b7dcbdcb83b7353a82
bige914 <ellsworth.evarts@gmail.com>
2021-04-01 12:26:27 -0400

    did some work on the distanceVector stuff, distance is now calculated, check lines 126 through 131 for what is left to complete

--- DistanceVectorRouter.java
+++ DistanceVectorRouter.java
@@ -5,7 +5,7 @@
  * Represents a router that uses a Distance Vector Routing algorithm.
  ***************/
 import java.util.ArrayList;
-import java.util.Hashmap;
+import java.util.HashMap;
 public class DistanceVectorRouter extends Router {
     // A generator for the given DistanceVectorRouter class
     public static class Generator extends Router.Generator {
@@ -27,16 +27,37 @@ public class DistanceVectorRouter extends Router {
         }
     }
 
+    private static class DistanceLink {
+        int distance;
+        int link;
+        public DistanceLink(int d, int l){
+            distance = d;
+            link = l;
+        }
+    }
+
     Debug debug;
-    
+    HashMap<Integer, DistanceLink> routingTable;
+    int []distances;
+    ArrayList<HashMap<Integer, DistanceLink>> neighborTables;
+
     public DistanceVectorRouter(int nsap, NetworkInterface nic) {
         super(nsap, nic);
         debug = Debug.getInstance();  // For debugging!
-         HashMap<int, int> routingTable = new HashMap<int, int>();
+         routingTable = new HashMap<>();
+        int size = nic.getOutgoingLinks().size();
+        distances = new int [size];
+        neighborTables = new ArrayList<>();
+
+        for(int i =0; i < size; i++){
+            distances[i]= Integer.MAX_VALUE;
+            neighborTables.add(new HashMap<>());
+        }
+
+        
     
         //new method to set array
-       //getOutgoinglinks is neighbors
-       //make ping packet, set timestamp and then calculate time diff when its received to get distance
+       
     
     }
         long delay = 1000;
@@ -44,8 +65,13 @@ public class DistanceVectorRouter extends Router {
         long timeToReCalc = System.currentTimeMillis() + delay;
 
         while (true) {
+
+        //getOutgoinglinks is neighbors
+        //make ping packet, set timestamp and then calculate time diff when its received to get distance
+
             if(System.currentTimeMillis() > timeToReCalc){
                  calcDistances();
+                 buildRoutingTable();
                  timeToReCalc = System.currentTimeMillis() + delay;
                 }  
             // See if there is anything to process
@@ -64,7 +90,8 @@ public class DistanceVectorRouter extends Router {
                 if(toRoute.data instanceof PingPacket){
                     //packet recieved
                     PingPacket p = (PingPacket) toRoute.data;
-                debug.println(3, "(DistanceVectorRouter.run): PingPacket Recieved! " + toRoute.originator + " to the destination: " + p.ping);
+                    processPingPacket(p, toRoute.originator);
+                
                     //if packet is recieved and ping is false, send ack, if false calc time stamp diff/2
                 }
             }
@@ -75,8 +102,36 @@ public class DistanceVectorRouter extends Router {
             }
         }
     }
+    private void processPingPacket(DistanceVectorRouter.PingPacket p, int originator) {
+        int linkIndex = nic.getOutgoingLinks().indexOf(originator);
+        debug.println(3, "(DistanceVectorRouter.run): PingPacket Recieved! " + originator + " to the destination: " + p.ping);
+        if (p.ping){
+            int distance = (int)(System.currentTimeMillis() - p.timestamp)/2;
+            distances[linkIndex]=distance;
+        }
+        else{
+            // mark it and send packet back
+            p.ping = true;
+            
+            nic.sendOnLink(linkIndex, p);
+        }
+    }
+    private void buildRoutingTable() {
+        //display array for debugging purposes
+        String msg = "Router " + nsap + ":";
+        for(int i=0; i< distances.length; i++){
+            msg = msg + " " + distances[i];
+        }
+        debug.println(5, msg);
+        //create a new tmp routing table
+        //add entry that is know, ergo distance to self = 0 and link index = -1
+        //use neighbor tables to build my table  
+        //send my table on link to neighbors (its a loop)
+        //make tmp table the new routing table
+        //for debugging, print out tmp table
+    }
     public void calcDistances(){
-        for(int i=0;i<toSend.getOutgoinglinks.size();i++){
+        for(int i=0;i<nic.getOutgoingLinks().size();i++){
             //make packet
             PingPacket p = new PingPacket();
             nic.sendOnLink(i,p);
